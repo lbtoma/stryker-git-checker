5239457b7930af6a39c06a9f8928ada5cd4e976a
Lucas Barioni Toma <lbtoma@gmail.com>
2021-08-30 00:11:41 -0300

    feat: add initial dummy plugin

--- spack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  entry: {
-    index: __dirname + "/src/index.ts",
-  },
-  output: {
-    path: __dirname + "/dist",
-  },
-};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-/** @todo */
+import { PluginKind, declareFactoryPlugin } from "@stryker-mutator/api/plugin";
+import { create } from "./git-checker";
+
+export const strykerPlugins = [
+  declareFactoryPlugin(PluginKind.Checker, "git-checker", create)
+];
+
+export const createGitChecker = create;
