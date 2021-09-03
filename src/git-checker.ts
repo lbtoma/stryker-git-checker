755adbdb1368e987edb0c1ebaf1dab6f67c028b2
Lucas Barioni Toma <lbtoma@gmail.com>
2021-09-03 03:21:56 -0300

    feat: add `parseDiffs` and use it in the Plugin

--- src/git-checker.ts
+++ src/git-checker.ts
@@ -8,11 +8,11 @@ import {
 } from "@stryker-mutator/api/plugin";
 import { Logger, LoggerFactoryMethod } from "@stryker-mutator/api/logging";
 import { Mutant } from "@stryker-mutator/api/core";
+import { DiffMap, parseDiffs } from "./helpers/diff-parser";
+import { exec } from "child_process";
 
-gitCheckerLoggerFactory.inject = tokens(
-  commonTokens.getLogger,
-  commonTokens.target
-);
+const GIT_DIFF_COMMAND = "git diff --color=never";
+
 function gitCheckerLoggerFactory(
   loggerFactory: LoggerFactoryMethod,
   target: Function | undefined
@@ -25,8 +25,17 @@ function gitCheckerLoggerFactory(
 
   return loggerFactory(category);
 }
+gitCheckerLoggerFactory.inject = tokens(
+  commonTokens.getLogger,
+  commonTokens.target
+);
 
-create.inject = tokens(commonTokens.injector);
+/**
+ * GitChecker factory.
+ *
+ * @param injector dependencies.
+ * @returns an instance of the plugin class.
+ */
 export function create(injector: Injector<PluginContext>): GitChecker {
   return injector
     .provideFactory(
@@ -36,18 +45,74 @@ export function create(injector: Injector<PluginContext>): GitChecker {
     )
     .injectClass(GitChecker);
 }
+create.inject = tokens(commonTokens.injector);
 
+/**
+ * Git Checker plugin class.
+ *
+ * @todo add integration tests.
+ */
 export class GitChecker implements Checker {
   public static inject = tokens(commonTokens.logger, commonTokens.options);
+  private diffMap: DiffMap | undefined;
 
   constructor(private readonly logger: Logger) {}
 
+  /**
+   * Initializes the Git Checker Plugin by executing
+   * the `git diff` command and parsing output.
+   *
+   * @returns Void Promise.
+   */
   public async init(): Promise<void> {
-    this.logger.info("Starting the Git Checker plugin");
+    this.logger.info("Starting the Git Checker plugin.");
+
+    return new Promise((resolve, reject) => {
+      exec(GIT_DIFF_COMMAND, (error, stdout, stderr) => {
+        if (error) {
+          this.logger.error(stderr);
+          this.logger.fatal("Error while executing the Git command.");
+          reject(error);
+        }
+
+        this.diffMap = parseDiffs(stdout);
+        this.logger.info("Git Checker plugin successfully loaded.");
+        resolve();
+      });
+    });
   }
 
-  public async check(mutant: Mutant): Promise<CheckResult> {
-    this.logger.info(mutant.fileName);
-    return { status: CheckStatus.Passed };
+  /**
+   * Checks if the mutant location has overlap with the Git diffs.
+   *
+   * @param mutant The mutant to be checked.
+   * @returns The Check Result.
+   */
+  public async check({ fileName, location }: Mutant): Promise<CheckResult> {
+    const diff = this.diffMap!.get(fileName);
+
+    if (diff) {
+      for (const [lineNumber, range] of diff.entries()) {
+        if (
+          // Start
+          (lineNumber === location.start.line &&
+            range[0] >= location.start.column) ||
+          // Middle
+          (lineNumber > location.start.line &&
+            lineNumber < location.end.line) ||
+          // Bottom
+          (lineNumber === location.end.line && range[1] < location.end.column)
+        ) {
+          return { status: CheckStatus.Passed };
+        }
+      }
+    }
+
+    // This cast is necessary because the status `Ignored` is missing
+    // in `@stryker-mutator/api` `CheckStatus` enum.
+    return {
+      status: "Ignored" as CheckStatus,
+      reason: "Mutation out of range",
+    };
   }
 }
--- tests/unit/helpers/diff-parser.spec.ts
+++ tests/unit/helpers/diff-parser.spec.ts
@@ -1,5 +1,4 @@
-import DiffParser from "@/helpers/diff-parser";
-import { Logger } from "@stryker-mutator/api/logging";
+import { compareLine, parseChunk } from "@/helpers/diff-parser";
 import { INVALID_CHUNK, VALID_CHUNK } from "@tests/mocks/chunks";
 
 describe("GitParser", () => {
@@ -8,7 +7,7 @@ describe("GitParser", () => {
       const deleted = "const fn = () => console.log('FOO');";
       const added = "const fn = () => console.log('FOO');";
 
-      const result = DiffParser.compareLine(deleted, added);
+      const result = compareLine(deleted, added);
 
       expect(result).toBe(undefined);
     });
@@ -17,43 +16,43 @@ describe("GitParser", () => {
       const deleted = "const fn = () => console.log('FOO');";
       const added = "const fn = () => console.log('BARBAZ');";
 
-      const result = DiffParser.compareLine(deleted, added);
+      const result = compareLine(deleted, added);
 
-      expect(result).toStrictEqual([30, 35]);
+      expect(result).toStrictEqual([31, 37]);
     });
 
     test("Right range", () => {
       const deleted = "const fn = () => console.log('FOO');";
       const added = "const fn = () => console.log('FOO'); // some comment";
 
-      const result = DiffParser.compareLine(deleted, added);
+      const result = compareLine(deleted, added);
 
-      expect(result).toStrictEqual([deleted.length, deleted.length + 15]);
+      expect(result).toStrictEqual([deleted.length + 1, deleted.length + 17]);
     });
 
     test("Left range", () => {
       const deleted = "const fn = () => console.log('FOO');";
       const added = "/* some comment */ const fn = () => console.log('FOO');";
 
-      const result = DiffParser.compareLine(deleted, added);
+      const result = compareLine(deleted, added);
 
-      expect(result).toStrictEqual([0, 18]);
+      expect(result).toStrictEqual([1, 20]);
     });
 
     test("New line", () => {
       const deleted = undefined;
       const added = "const fn = () => console.log('FOO');";
 
-      const result = DiffParser.compareLine(deleted, added);
+      const result = compareLine(deleted, added);
 
-      expect(result).toStrictEqual([0, added.length - 1]);
+      expect(result).toStrictEqual([1, added.length + 1]);
     });
 
     test("Empty line", () => {
       const deleted = "const fn = () => console.log('FOO');";
       const added = "";
 
-      const result = DiffParser.compareLine(deleted, added);
+      const result = compareLine(deleted, added);
 
       expect(result).toStrictEqual(undefined);
     });
@@ -62,35 +61,31 @@ describe("GitParser", () => {
       const deleted = "const fn = () => console.log('FOO');";
       const added = "let a = 0";
 
-      const result = DiffParser.compareLine(deleted, added);
+      const result = compareLine(deleted, added);
 
-      expect(result).toStrictEqual([0, added.length - 1]);
+      expect(result).toStrictEqual([1, added.length + 1]);
     });
   });
 
   describe("parseChunk", () => {
     test("Valid chunk", () => {
-      const parser = new DiffParser({ error: () => {} } as unknown as Logger);
+      const rangeMap = parseChunk(VALID_CHUNK, 12);
 
-      const rangeMap = parser.parseChunk(VALID_CHUNK, 12);
-
-      expect(rangeMap.get(12)).toBe(undefined);
-      expect(rangeMap.get(13)).toBe(undefined);
-      expect(rangeMap.get(14)).toBe(undefined);
-      expect(rangeMap.get(15)).toStrictEqual([10, 31]);
-      expect(rangeMap.get(16)).toBe(undefined);
-      expect(rangeMap.get(17)).toBe(undefined);
-      expect(rangeMap.get(18)).toStrictEqual([36, 36]);
-      expect(rangeMap.get(19)).toStrictEqual([0, 27]);
-      expect(rangeMap.get(20)).toBe(undefined);
-      expect(rangeMap.get(21)).toBe(undefined);
+      expect(rangeMap.find(([n]) => n === 12)).toBe(undefined);
+      expect(rangeMap.find(([n]) => n === 13)).toBe(undefined);
+      expect(rangeMap.find(([n]) => n === 14)).toBe(undefined);
+      expect(rangeMap.find(([n]) => n === 15)).toStrictEqual([15, [11, 33]]);
+      expect(rangeMap.find(([n]) => n === 16)).toBe(undefined);
+      expect(rangeMap.find(([n]) => n === 17)).toBe(undefined);
+      expect(rangeMap.find(([n]) => n === 18)).toStrictEqual([18, [37, 38]]);
+      expect(rangeMap.find(([n]) => n === 19)).toStrictEqual([19, [1, 29]]);
+      expect(rangeMap.find(([n]) => n === 20)).toBe(undefined);
+      expect(rangeMap.find(([n]) => n === 21)).toBe(undefined);
     });
 
     test("Invalid chunk", () => {
-      const parser = new DiffParser({ error: () => {} } as unknown as Logger);
-
       expect(() => {
-        parser.parseChunk(INVALID_CHUNK, 12);
+        parseChunk(INVALID_CHUNK, 12);
       }).toThrow('Unexpected chunk prefix = "');
     });
   });
